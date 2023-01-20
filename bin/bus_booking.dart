1e045e0192f4e6858a3f27a5a717bc72c04658ae
Christen Saad <erimano837@gmail.com>
2023-01-20 20:12:12 +0200

    intial project

---
+++ bin/bus_booking.dart
@@ -0,0 +1,5 @@
+import 'package:bus_booking/bus_booking.dart' as bus_booking;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bus_booking.calculate()}!');
+}
---
+++ test/bus_booking_test.dart
@@ -0,0 +1,8 @@
+import 'package:bus_booking/bus_booking.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
